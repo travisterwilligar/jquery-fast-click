0cfadc6cd5552b5af67b396017b07bb599b038bc
Flo Unternährer <florian.unternaehrer@sysinf.ch>
2012-06-28 22:33:37 +0200

    if there's no clickhandler on the element, fastclick looks for href... UNTESTED

--- jQuery.fastClick.js
+++ jQuery.fastClick.js
@@ -17,128 +17,134 @@
 
 (function($) {
 
-$.fn.fastClick = function(options) {
-	options = options || {};
-	return $(this).each(function() {
-		var button = $(this)[0],
-				clickEvent = null;
+	$.fn.fastClick = function(options) {
+		options = options || {};
+		return $(this).each(function() {
+			var button = $(this)[0],
+					clickEvent = null;
 
-		if (options.handler == null) {
-			if (button.onclick instanceof Function) {
-				clickEvent = button.onclick;
-				button.onclick = '';
-			}
-		}
-
-		if (options.handler != null || clickEvent != null) {
-			$.FastButton(button, options.handler || clickEvent, options);
-		}
-	});
-};
-
-$.FastButton = function(element, handler, options) {
-	var startX, startY;
-
-	var reset = function() {
-		$(element).unbind('touchend');
-		$("body").unbind('touchmove.fastClick');
-		delayManager.stop();
-	};
-
-	var onClick = function(event) {
-		event.stopPropagation();
-		delayManager.running && delayManager.stop() || handler.call(this, event);
-		reset();
-
-		if (event.type === 'touchend') {
-			$.clickbuster.preventGhostClick(startX, startY);
-		}
-	};
-
-	var onTouchMove = function(event) {
-		if (Math.abs(event.originalEvent.touches[0].clientX - startX) > 10 ||
-			Math.abs(event.originalEvent.touches[0].clientY - startY) > 10) {
-			reset();
-		}
-	};
-
-	var onTouchStart = function(event) {
-		event.stopPropagation();
-
-		$(element).bind('touchend', onClick);
-		$("body").bind('touchmove.fastClick', onTouchMove);
-
-		startX = event.originalEvent.touches[0].clientX;
-		startY = event.originalEvent.touches[0].clientY;
-
-		// keep firing handler if option "permanent" is set
-		if (options && options.permanent) {
-			delayManager.start(element, handler);
-		}
-	};
-
-	$(element).unbind();
-	$(element).bind({
-		touchstart: onTouchStart,
-		mouseover: function() { return false; },
-		click: onClick
-	});
-
-	var delayManager = (function() {
-		var delay = 300,
-				applyHandler;
-
-		this.start = function(el, handler) {
-			// initialize applyHandler everytime start gets called
-			applyHandler = function(ms) {
-				if (this.running) {
-					ms = ms || delay; // TODO: ease the shit out of this
-					window.setTimeout(applyHandler, ms);
-					handler.call(el);
+			if (options.handler == null) {
+				if (button.onclick instanceof Function) {
+					// clickeventd
+					clickEvent = button.onclick;
+					button.onclick = '';
+				} else if (button.attr('href') != null) {
+					// link
+					clickEvent = function() {
+						window.location.href = button.attr('href');
+					}
 				}
+			}
+
+			if (options.handler != null || clickEvent != null) {
+				$.FastButton(button, options.handler || clickEvent, options);
+			}
+		});
+	};
+
+	$.FastButton = function(element, handler, options) {
+		var startX, startY;
+
+		var reset = function() {
+			$(element).unbind('touchend');
+			$("body").unbind('touchmove.fastClick');
+			delayManager.stop();
+		};
+
+		var onClick = function(event) {
+			event.stopPropagation();
+			delayManager.running && delayManager.stop() || handler.call(this, event);
+			reset();
+
+			if (event.type === 'touchend') {
+				$.clickbuster.preventGhostClick(startX, startY);
+			}
+		};
+
+		var onTouchMove = function(event) {
+			if (Math.abs(event.originalEvent.touches[0].clientX - startX) > 10 ||
+				Math.abs(event.originalEvent.touches[0].clientY - startY) > 10) {
+				reset();
+			}
+		};
+
+		var onTouchStart = function(event) {
+			event.stopPropagation();
+
+			$(element).bind('touchend', onClick);
+			$("body").bind('touchmove.fastClick', onTouchMove);
+
+			startX = event.originalEvent.touches[0].clientX;
+			startY = event.originalEvent.touches[0].clientY;
+
+			// keep firing handler if option "permanent" is set
+			if (options && options.permanent) {
+				delayManager.start(element, handler);
+			}
+		};
+
+		$(element).unbind();
+		$(element).bind({
+			touchstart: onTouchStart,
+			mouseover: function() { return false; },
+			click: onClick
+		});
+
+		var delayManager = (function() {
+			var delay = 300,
+					applyHandler;
+
+			this.start = function(el, handler) {
+				// initialize applyHandler everytime start gets called
+				applyHandler = function(ms) {
+					if (this.running) {
+						ms = ms || delay; // TODO: ease the shit out of this
+						window.setTimeout(applyHandler, ms);
+						handler.call(el);
+					}
+				};
+
+				// kick off
+				window.setTimeout(applyHandler, delay);
+				this.running = true;
 			};
 
-			// kick off
-			window.setTimeout(applyHandler, delay);
-			this.running = true;
-		};
+			this.stop = function() {
+				this.running = false;
+				applyHandler = null;
+			};
 
-		this.stop = function() {
-			this.running = false;
-			applyHandler = null;
-		};
+			return this;
+		})();
+	};
 
-		return this;
-	})();
-};
+	$.clickbuster = {
+		coordinates: [],
 
-$.clickbuster = {
-	coordinates: [],
+		preventGhostClick: function(x, y) {
+			$.clickbuster.coordinates.push(x, y);
+			window.setTimeout($.clickbuster.pop, 2500);
+		},
 
-	preventGhostClick: function(x, y) {
-		$.clickbuster.coordinates.push(x, y);
-		window.setTimeout($.clickbuster.pop, 2500);
-	},
+		pop: function() {
+			$.clickbuster.coordinates.splice(0, 2);
+		},
 
-	pop: function() {
-		$.clickbuster.coordinates.splice(0, 2);
-	},
-
-	onClick: function(event) {
-		var x, y, i;
-		for (i = 0; i < $.clickbuster.coordinates.length; i += 2) {
-			x = $.clickbuster.coordinates[i];
-			y = $.clickbuster.coordinates[i + 1];
-			if (Math.abs(event.clientX - x) < 25 && Math.abs(event.clientY - y) < 25) {
-				event.stopPropagation();
-				event.preventDefault();
+		onClick: function(event) {
+			var x, y, i;
+			for (i = 0; i < $.clickbuster.coordinates.length; i += 2) {
+				x = $.clickbuster.coordinates[i];
+				y = $.clickbuster.coordinates[i + 1];
+				if (Math.abs(event.clientX - x) < 25 && Math.abs(event.clientY - y) < 25) {
+					event.stopPropagation();
+					event.preventDefault();
+				}
 			}
 		}
-	}
-};
+	};
 
-$(function(){
-	document.addEventListener('click', $.clickbuster.onClick, true);
-});
+	$(function(){
+		document.addEventListener('click', $.clickbuster.onClick, true);
+	});
 
 }(jQuery));
